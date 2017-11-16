17dbd0e6c014c1dc45f178e7556e8b496dc3c2ff
DarielJ <DarielJ@10.21.34.172>
2017-11-16 17:22:28 -0400

    Centered JFrame

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,17 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Dariel J.");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocationRelativeTo(null);
-		myFrame.setSize(800, 500);
+		
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		myFrame.setLocation((int)screen.getWidth()/2 -(350/2), (int)screen.getHeight()/2-(250/2));
+		myFrame.setSize(350, 250);
+		
         MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
