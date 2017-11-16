2538299e0779bf729c2c37dc4c68366b3c0abac2
DarielJ <DarielJ@DJ>
2017-11-15 21:34:24 -0400

    Centralize

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Dariel J.");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
+		myFrame.setLocationRelativeTo(null);
 		myFrame.setSize(200, 200);
         MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
